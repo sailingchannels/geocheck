49f4e4f8ff854e4b49c5a3b407428d0ca548ad94
Thomas Brüggemann <mail@thomasbrueggemann.com>
2016-11-29 00:01:50 +0000

    classifier ready

--- classifier.py
+++ classifier.py
@@ -1,10 +1,14 @@
 from pymongo import MongoClient
-import config, sys, geograpy2
+import config, sys, time, requests, json
+from geopy.geocoders import Nominatim
 
 # open mongodb connection
 client = MongoClient(config.mongoDB())
 db_name = "sailing-channels"
 devMode = False
+
+# init geolocator
+geolocator = Nominatim()
 
 # check if dev mode is active
 if len(sys.argv) != 2:
@@ -14,15 +18,53 @@ if len(sys.argv) != 2:
 
 db = client[db_name]
 
-# loop all videos that do not have any geotags
-for vid in db.videos.find({"geo": {"$exists": False}}, projection=["_id", "title", "description"]):
+# loop all videos that do not have any geotags and are younger than 1 year
+videos = db.videos.find({"geoChecked": {"$exists": False}}, projection=["_id", "title", "description"], limit=9900)
+print videos.count()
+
+for vid in videos:
 
 	txt = vid["title"] + " " + vid["description"]
-	print txt
 
 	# try to get some place information
-	r = requests.post("http://httpbin.org/post", data=payload)
+	headers = {
+		"X-RosetteAPI-Key": config.apiKey(),
+		"Content-Type": "application/json",
+		"Accept": "application/json"
+	}
+	payload = {"content": txt}
+	r = requests.post("https://api.rosette.com/rest/v1/entities", headers=headers, data=json.dumps(payload))
 
-	print vid["_id"]
-	print "- - -"
-	break
+	result = r.json()
+
+	# find location tags
+	locations = []
+	if result.has_key("entities"):
+		for entity in result["entities"]:
+			if entity["type"] == "LOCATION":
+
+				location = geolocator.geocode(entity["normalized"])
+				if location:
+
+					print vid["_id"], entity["normalized"], location.longitude, location.latitude
+
+					locations.append({
+						"type": "Point",
+						"coordinates": [
+							location.longitude,
+							location.latitude
+						]
+					})
+
+		if len(locations) > 0:
+			db.videos.update_one({"_id": vid["_id"]}, {"$set": {
+				"geo": {
+					"type": "GeometryCollection",
+	  				"geometries": locations
+				},
+				"geoChecked": True
+			}})
+		else:
+			db.videos.update_one({"_id": vid["_id"]}, {"$set": {
+				"geoChecked": True
+			}})
--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 sudo pip install -r requirements.txt --ignore-installed six
-sudo pip install git+https://github.com/Corollarium/geograpy2.git --ignore-installed six
